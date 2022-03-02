bc3f621f66d729cb76a6c7b901178cb376a2ab10
Sorabh3127 <sorabh.cu.2001@gmail.com>
2022-03-02 20:47:08 +0530

    donr

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -6,7 +6,7 @@ export const client = sanityClient({
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token: 'skaCZEhT73lbhwWmhCjkc4aSnOm5wkTntL6omTI6C5vRS5mkcN1NVBm5GiNBZ9KSsfIBAI6QtOhM2fiXqmkK7E9zw4XeWarOpGDc8tyZAly0J76RDWLhdXHw243iw7pJqzZ1cSKd11sERWV5TrJaa5rrOIB0icMPDs9u1D0wUwmHpHiaN0Uf',
 });
 
 const builder = imageUrlBuilder(client);
